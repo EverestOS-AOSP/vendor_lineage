ProjectEverest/vendor_everest

vendor: Import our Bootanimation
Special thanks to:
@SudipRajbanshi - for logo
@ztrahmet - for animation
@XelXen - for fixes

config/common.mk
@@ -62,6 +62,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Bootanimation
+include vendor/everest/config/bootanimation.mk
+
 # Call Recording
 TARGET_CALL_RECORDING_SUPPORTED ?= true
 ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)

EverestOS-AOSP/vendor_lineage

config/common.mk
@@ -266,3 +266,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk